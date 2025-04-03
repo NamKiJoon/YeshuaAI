1b2de3a9521bbea6afa9382f95d8b82c1518d536
jerry <jerry@acryl.ai>
2025-04-03 15:24:16 +0900

    Feat: params에 따른 말씀 꺼내기 추가

--- src/app/bible/[book]/[chapter]/[verse]/page.tsx
+++ src/app/bible/[book]/[chapter]/[verse]/page.tsx
@@ -1,32 +1,42 @@
 import bibleData from "@/data/bible.json";
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 import { ClientBible } from "@/components/bible/ClientBible";
 
-// interface Params {
-//   params: {
-//     book: string;
-//     chapter: string;
-//     verse: string;
-//   };
-// }
+interface Params {
+  params: {
+    book: string;
+    chapter: string;
+    verse: string;
+  };
+}
 
-const Page = () => {
-  // const { book, chapter, verse } = await params;
-  // const key = `${book}${chapter}:${verse}`;
-  // const verseText = key
+const Page = async ({ params }: Params) => {
+  const { book, chapter, verse } = await params;
 
-  // const a = Object.keys(bibleData).map((x,i)=>{
-  //   if (key == x){
-  //     console.log(x)
-  //   }
-  // })
-  // console.log(Object.keys(bibleData))
+  const decodeBook = decodeURIComponent(book);
+  const getValue = `${decodeBook}${chapter}:${verse}`;
+
+  const typeBible = bibleData as Record<string, string>;
+
+  const a = Object.keys(bibleData).map((x, i) => {
+    if (getValue == x) {
+      return typeBible[getValue];
+    }
+  });
+  // console.log(Object.keys(bibleData));
 
   // url의 json 키값과 매핑하여 컴포넌트에 전달
   // console.log('Keys =',decodeURIComponent(key))
 
   // return <ClientBible text={decodeURIComponent(verseText)}/>
-  return null
+  return (
+    <div>
+      {a}
+      {decodeBook}
+      {chapter}
+      {verse}
+    </div>
+  );
 };
 
 export default Page;
