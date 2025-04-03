bde43cb71ac962379906f82441ebb569b686e071
jerry <jerry@acryl.ai>
2025-04-03 15:23:59 +0900

    Feat: 개발 서버 allow 허용

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   // output:'export',
   // basePath: '/promoweb',
+  allowedDevOrigins: ["http://localhost:3000", "http://172.29.123.184:5173"],
 };
 
 export default nextConfig;
