623cb8237bcc8323897fb204789354f6f8c90508
Namkijoon <swms22@naver.com>
2025-04-03 09:23:48 +0900

    Git Test

--- src/app/bible/[book]/[chapter]/[verse]/page.tsx
+++ src/app/bible/[book]/[chapter]/[verse]/page.tsx
@@ -2,18 +2,18 @@ import bibleData from "@/data/bible.json";
 import { notFound } from 'next/navigation';
 import { ClientBible } from "@/components/bible/ClientBible";
 
-interface Params {
-  params: {
-    book: string;
-    chapter: string;
-    verse: string;
-  };
-}
+// interface Params {
+//   params: {
+//     book: string;
+//     chapter: string;
+//     verse: string;
+//   };
+// }
 
-const Page = async({ params }: Params) => {
-  const { book, chapter, verse } = await params;
-  const key = `${book}${chapter}:${verse}`;
-  const verseText = key
+const Page = () => {
+  // const { book, chapter, verse } = await params;
+  // const key = `${book}${chapter}:${verse}`;
+  // const verseText = key
 
   // const a = Object.keys(bibleData).map((x,i)=>{
   //   if (key == x){
@@ -23,9 +23,10 @@ const Page = async({ params }: Params) => {
   // console.log(Object.keys(bibleData))
 
   // url의 json 키값과 매핑하여 컴포넌트에 전달
-  console.log('Keys =',decodeURIComponent(key))
+  // console.log('Keys =',decodeURIComponent(key))
 
-  return <ClientBible text={decodeURIComponent(verseText)}/>
+  // return <ClientBible text={decodeURIComponent(verseText)}/>
+  return null
 };
 
 export default Page;
