1afda02da156d3a432cb805552159fff936533bc
jerry <swms22@naver.com>
2025-04-14 10:34:10 +0900

    Git Test

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,8 +2,8 @@ import { SelectBible } from "@/components/search/SelectBible";
 
 const SearchPage = () => {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">말씀 검색</h1>
+    <main className="">
+      <h1 className="text-2xl font-bold mb-4 border-2">검색</h1>
       <SelectBible />
     </main>
   );
